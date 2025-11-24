d8fe66985a93627c120d5c3a88184a43a700c6d2
nbhaskarreddy <nallamillibhaskarreddy@gmail.com>
2025-11-24 21:34:04 +0530

    conditions

--- 11-condition.sh
+++ 11-condition.sh
@@ -3,6 +3,7 @@
 #condition for count >10 or <10
 Number=$1
 
+
 if [ "$Number" -lt 10 ]; then
     echo "$Number is Less to 10"
 elif [ "$Number" -eq 10 ]; then
@@ -10,3 +11,12 @@ elif [ "$Number" -eq 10 ]; then
 else 
     echo "$Number is greater than 10"
 fi
+
+echo "Please enter the number"
+
+read Number2
+if [ $(($Number2%2)) -eq 0 ]; then
+    echo "$Number2 is Even"
+else 
+    echo "$Number2 is odd"
+fi        
