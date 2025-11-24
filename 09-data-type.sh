237adac3df7bb1fe11778f8abb4b659fb137797a
nbhaskarreddy <nallamillibhaskarreddy@gmail.com>
2025-11-24 18:56:40 +0530

    conversation

---
+++ 09-data-type.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Everything in shell considered as String
+NUMBER1=100
+NUMBER2=200
+NAME=DEVOPS
+
+#NAME AS STRING IS CONSIDER AS ZERO AND IT WILL PRINT OUTPUT
+SUM=$((NUMBER1+NUMBER2+NAME))
+
+echo "sum of numbers ${SUM}"
