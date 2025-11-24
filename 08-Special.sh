12586a8cd99805fd3f0e1adff86aabbb34e2c9ee
nbhaskarreddy <nallamillibhaskarreddy@gmail.com>
2025-11-24 18:17:41 +0530

    conversation

---
+++ 08-Special.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "All variables passed to the script: $@"
+echo "All variables passed to the script: $*"
+echo "All variables passed to the script: $0"
+echo "All variables passed to the script: $pwd"
+echo "All variables passed to the script: $user"
+echo "All variables passed to the script: $home"
+echo "All variables passed to the script: $$"
