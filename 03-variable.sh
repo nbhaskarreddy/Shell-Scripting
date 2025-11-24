bd45a02af87f3d83e663a6fb90c61b855aab9c4d
nbhaskarreddy <nallamillibhaskarreddy@gmail.com>
2025-11-24 17:11:17 +0530

    conversation

---
+++ 03-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+#No space between variable and value
+PERSON1=JOHN
+PERSON2=ROCKY
+
+echo "$PERSON1:: Hi $PERSON2,How are you"
+echo "$PERSON2:: Hello $PERSON1, Iam fine, How are you"
+echo "$PERSON1:: I am good, Are you learning any thing new"
+echo "$PERSON2:: I am learning Devops"
