283a817dc0e0924f9d66968a5a0288ce1cd3085a
nbhaskarreddy <nallamillibhaskarreddy@gmail.com>
2025-11-24 17:18:35 +0530

    conversation

---
+++ 04-argument.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1 
+PERSON2=$2
+
+echo "${PERSON1}:: Welcome to Alaska ${PERSON2}"
+echo "${PERSON2}:: Thank you ${PERSON1}, Finally we meet directly"
+echo "${PERSON1}:: can we have a deal now"
+echo "${PERSON2}:: Depends on the quo status"
