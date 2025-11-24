e29e24654da385954a41b81678f51218aef493d9
nbhaskarreddy <nallamillibhaskarreddy@gmail.com>
2025-11-24 16:58:45 +0530

    conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi Suresh,How are you"
+echo "Suresh:: Hello Ramesh, Iam fine, How are you"
+echo "Ramesh:: I am good, Are you learning any thing new"
+echo "Suresh:: I am learning Devops"
