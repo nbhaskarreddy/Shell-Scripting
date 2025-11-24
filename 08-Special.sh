30977efd15056ab3cfb568694e65a7feb0d75433
nbhaskarreddy <nallamillibhaskarreddy@gmail.com>
2025-11-24 18:22:58 +0530

    conversation

--- 08-Special.sh
+++ 08-Special.sh
@@ -7,3 +7,7 @@ echo "All variables passed to the script: $PWD"
 echo "All variables passed to the script: $USER"
 echo "All variables passed to the script: $HOME"
 echo "All variables passed to the script: $$"
+
+sleep 50 &
+
+echo "PID of the last command in background is: $!"
