509b31191cf6895aec3566204bddf4b5be5b94be
nbhaskarreddy <nallamillibhaskarreddy@gmail.com>
2025-11-24 19:07:32 +0530

    conversation

---
+++ 10-index.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#size of array 3 max index :3
+# LEADERS=("MODI" "PUTIN" "TRUMP")
+LEADERS=("$1" "$2" "$3")
+echo "All leaders:${LEADERS[0]}"
+echo "All leaders:${LEADERS[1]}"
+echo "All leaders:${LEADERS[2]}"
