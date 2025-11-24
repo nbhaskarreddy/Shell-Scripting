791627f89544c8bb9dbf0f5b881b2feec65e610c
nbhaskarreddy <nallamillibhaskarreddy@gmail.com>
2025-11-24 18:11:28 +0530

    conversation

--- 07-Date.sh
+++ 07-Date.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-DATE=$(date)
+#DATE=$(date)
 
-echo "Timestamp executed $DATE"
+START_TIME=$(date +%s)
+
+sleep 10
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME - $START_TIME))
+echo "Timestamp executed $TOTAL_TIME"
