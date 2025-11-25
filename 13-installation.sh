03ccdb0f2e8d70c4d591385cf554e774ba12b006
nbhaskarreddy <nallamillibhaskarreddy@gmail.com>
2025-11-25 13:59:24 +0530

    installation

---
+++ 13-installation.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0]; then
+    echo "Please run script with root user"
+    exit 127
+fi
+
+validate() {
+    if [ $1 -ne 0]; then
+    echo "Failure in installing... $2"
+    exit 1
+else 
+    echo "Successful installing... $2"
+fi
+}
+
+dnf install nginx -y
+validate $? nginx
+
+dnf install mysql -y
+validate $? Mysql
+
+dnf install python3 -y
+validate $? Python3
