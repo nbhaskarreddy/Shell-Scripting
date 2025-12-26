f6bed436a73f325b0a4399f51202f595bfce50bb
nbhaskarreddy <nallamillibhaskarreddy@gmail.com>
2025-12-26 22:49:46 +0530

    Colors.sh update

--- 16-colors.sh
+++ 16-colors.sh
@@ -4,6 +4,6 @@ R=\e[32m
 N=\e[37m
 G=\e[31m
 Y=\e[33m
-echo -e "$G Success $N"
-echo -e "$R Failure $N"
-echo -e "$Y Skipping $N"
+echo -e"$G Success $N"
+echo -e"$R Failure $N"
+echo -e"$Y Skipping $N"
