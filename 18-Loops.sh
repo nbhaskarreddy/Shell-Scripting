92400d5ba6b5b88fd20cd0755fa8d62b80c53721
nbhaskarreddy <nallamillibhaskarreddy@gmail.com>
2025-12-27 15:50:43 +0530

    Loops

--- 18-Loops.sh
+++ 18-Loops.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+LOG_DIR="/var/log/shell_script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOG_DIR/$SCRIPT_NAME.log"
+
 for i in {1..25}
 do
-    echo $i
+    echo $i &>>$LOG_FILE
 done
